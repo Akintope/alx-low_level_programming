e8ce5df2e4bf18c126e385f011471703e500d06d
Akintope <akinmoro@gmail.com>
2023-10-10 16:07:37 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,15 +10,12 @@ int main(void)
     int count;
     unsigned long fib1 = 1, fib2 = 2, next;
 
-    printf("%lu, %lu, ", fib1, fib2);
+    printf("%lu, %lu", fib1, fib2);
 
     for (count = 3; count <= 98; count++)
     {
         next = fib1 + fib2;
-        printf("%lu", next);
-
-        if (count != 98)
-            printf(", ");
+        printf(", %lu", next);
 
         fib1 = fib2;
         fib2 = next;
