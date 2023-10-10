b6d1de74a22728b8eed5cd4424159cfc5edc3166
Akintope <akinmoro@gmail.com>
2023-10-10 13:15:59 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,23 @@
 #include "main.h"
 
 /**
-  * main -this is a function
-  * Return: returns 0
-  */
- int main(void)
- {
-  _putchar('_');
-  _putchar('p');
-  _putchar('u');
-  _putchar('t');
-  _putchar('c');
-  _putchar('h');
-  _putchar('a');
-  _putchar('r');
-  _putchar('\n');
+ * main - function
+ * Return: 0
+ */
 
- return (0);
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 
- }
-~
-~
--- VISUAL BLOCK 
+}
+
+
