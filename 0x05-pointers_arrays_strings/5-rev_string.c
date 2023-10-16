5f27f93815ade12f7a8997475a6b818614109330
Akintope <akinmoro@gmail.com>
2023-10-16 16:26:41 -0700

    5-rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  * rev_string - name of the function
+  * @s: the function parameter
+  */
+
+void rev_string(char *s)
+{
+int i;
+int total = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+	total++;
+
+for (i = 0; i < total / 2; i++)
+{
+char j;
+j = s[i];
+s[i] = s[count - 1 - i];
+s[count - 1 - i] = j;
+}
+}
