3a21a5fac6d35e2d3f0ad7262369ca8d58fc4695
Akintope <akinmoro@gmail.com>
2023-10-25 15:06:12 -0700

    1-print_rev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * _print_rev_recursion - is the main function
+  *
+  * @s: is the function parameter
+  *
+  * Return: 0
+  */
+void _print_rev_recursion(char *s)
+{
+if (*s)
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+}
