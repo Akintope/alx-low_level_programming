654439cf6558301784af2d48a961c3c9ccc05b83
Akintope <akinmoro@gmail.com>
2023-10-27 14:12:49 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * main - it prints the number of arguments passed down to the program
  * @argc: is the number of arguments
  * @argv: is the array of arguments
- * Return: Always 0 		
+ * Return: Always 0
  */
 
 int main(int argc, char *argv[])
