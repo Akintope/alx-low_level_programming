f5505632c1d99e501d38b1df03a12c125e7d1d48
Akintope <akinmoro@gmail.com>
2023-11-02 16:37:38 -0700

    100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * simple_print_buffer - it prints buffer
+ * @buffer: is the address of memory
+ * @size: is the size of the memory
+ * Return: 0
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+unsigned int k;
+
+k = 0;
+while (k < size)
+{
+if (k % 10)
+{
+printf(" ");
+}
+if (!(k % 10) && k)
+{
+printf("\n");
+}
+printf("0x%02x", buffer[k]);
+k++;
+}
+printf("\n");
+}
+
+/**
+ * main - check the code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char *p;
+int k;
+
+p = malloc(sizeof(char) * 10);
+p = _realloc(p, sizeof(char) * 10, sizeof(char) * 98);
+k = 0;
+while (k < 98)
+{
+p[k++] = 98;
+}
+simple_print_buffer(p, 98);
+free(p);
+return (0);
+}
