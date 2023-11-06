a81be0277ad2f200b4e7faca51c60c1b02786350
Akintope <akinmoro@gmail.com>
2023-11-06 15:35:45 -0800

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef MACROLIKE_H
+#define MACROLIKE_H
+/**
+ * 3-function_like_maacro: is the file
+ * @x: the input value (an int)
+ * Description: the header file that computes absolute value
+ */
+#define ABS(x) ((x) < 0 ? -(1) * (x) : (x))
+
+#endif
