7c7b2366760230ccc1bff213b92dd1798a8fedaa
Akintope <akinmoro@gmail.com>
2023-11-06 15:39:32 -0800

    4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,10 @@
+#ifndef SUM_H
+#define SUM_H
+/**
+ * 4-sum: the file
+ * @x: the first int input
+ * @y: the second int input
+ * Description: to sum up x and y
+ */
+#define SUM(x, y) (x + y)
+#endif
