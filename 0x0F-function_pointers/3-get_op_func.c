5c2a65c12fc40997fa1778b8a23d73c66c05d8f2
Akintope <akinmoro@gmail.com>
2023-11-08 15:57:16 -0800

    3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,56 +1,30 @@
+
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
+	#include <stdlib.h>
+	/**
+	 * get_op_func - Its selects the correct function to perform
+	 *               the operation asked by the user.
+	 * @s: Is the operator passed as argument.
+	 * Return: 0
+	 */
+	int (*get_op_func(char *s))(int, int)
+	{
+		op_t ops[] = {
+			{"+", op_add},
+			{"-", op_sub},
+			{"*", op_mul},
+			{"/", op_div},
+			{"%", op_mod},
+			{NULL, NULL},
+		};
 
-/**
- * get_op_func - it selects the option to perform arithmetic op
- * @s: is the operator argument
- * Return: 0
- */
-int (*get_op_func(char *s))(int, int)
-{
-op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
-};
-int i;
 
-i = 0;
+		int i = 0;
 
-while (ops[i].op != NULL && *(ops[i].op) != *s)
-i++;
 
-return (ops[i].f); /* return operation */
-#include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
+		while (ops[i].op != NULL && *(ops[i].op) != *s)
+			i++;
 
-/**
- * get_op_func - select the option to perform arithmetic op
- * @s: operator argument
- *
- * Return: result of selected choice
- */
-int (*get_op_func(char *s))(int, int)
-{
-op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
-};
-int i;
 
-i = 0;
-
-while (ops[i].op != NULL && *(ops[i].op) != *s)
-i++;
-
-return (ops[i].f);
-}
+		return (ops[i].f);
+	}
