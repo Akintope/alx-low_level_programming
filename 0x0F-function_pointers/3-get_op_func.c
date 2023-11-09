ce4d796acdd4104701806d20e57d23a49142cbd9
Akintope <akinmoro@gmail.com>
2023-11-08 16:14:52 -0800

    3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,30 +1,30 @@
-
 #include "3-calc.h"
-	#include <stdlib.h>
-	/**
-	 * get_op_func - Its selects the correct function to perform
-	 *               the operation asked by the user.
-	 * @s: Is the operator passed as argument.
-	 * Return: 0
-	 */
-	int (*get_op_func(char *s))(int, int)
-	{
-		op_t ops[] = {
-			{"+", op_add},
-			{"-", op_sub},
-			{"*", op_mul},
-			{"/", op_div},
-			{"%", op_mod},
-			{NULL, NULL},
-		};
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * get_op_func - select the option to perform arithmetic op
+ * @s: operator argument
+ *
+ * Return: 0
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
 
 
-		int i = 0;
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
 
-
-		while (ops[i].op != NULL && *(ops[i].op) != *s)
-			i++;
-
-
-		return (ops[i].f);
-	}
+	return (ops[i].f);
+}
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,17 +2,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "3-calc.h"
-	/**
+/**
 * main - It prints the result of simple operations.
 * @argc: It is the number of arguments supplied to the program.
 * @argv: An array of pointers to the arguments.
 * Return: 0
 */
+
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 int num1, num2;
 char *op;
-
+}
 
 if (argc != 4)
 {
@@ -46,4 +47,3 @@ printf("%d\n", get_op_func(op)(num1, num2));
 
 return (0);
 }
-
