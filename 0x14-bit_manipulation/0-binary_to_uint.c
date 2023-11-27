3d3890a19667f848b430753d8c5e0bcade1d29d9
Akintope <akinmoro@gmail.com>
2023-11-27 05:23:19 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,26 +1,27 @@
 #include "main.h"
 /**
- * binary_to_uint - its a function that converts a binary number to unsigned int.
- * @b:is a  pointer to a string containing a binary number
- * Return: unsigned int with decimal value of the binary number
- */
+* binary_to_uint - its a function that converts a binary number to unsigned int.
+* @b:is a  pointer to a string containing a binary number
+* Return: unsigned int with decimal value of the binary number
+*/
 unsigned int binary_to_uint(const char *b)
 {
-int i;
-unsigned int num;
-num = 0;
-if (!j)
-return (0);
-for (i = 0; j[i] != '\0'; i++)
-{
-if (j[i] != '0' && j[i] != '1')
-return (0);
-}
-for (i = 0; j[i] != '\0'; i++)
-{
-num <<= 1;
-if (j[i] == '1')
-num += 1;
-}
-return (num);
+	int a;
+	unsigned int num;
+
+	num = 0;
+	if (!b)
+		return (0);
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		if (b[a] != '0' && b[a] != '1')
+			return (0);
+	}
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		num <<= 1;
+		if (b[a] == '1')
+			num += 1;
+	}
+	return (num);
 }
